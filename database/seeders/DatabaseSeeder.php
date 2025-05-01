c6b6185b59526864716619f97389eb9843e353ba
dhabyap <dhabyap@gmail.com>
2025-05-01 09:14:30 +0000

    di server

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\unit;
+use App\Models\Unit;
 use App\Models\User;
 use App\Models\Level;
 use App\Models\Appartement;
